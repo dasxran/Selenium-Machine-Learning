139ad835a318339c8d440842d0aa2d67e8189449
Ranjan Das <ranjan@gitlab.com>
2019-05-28 02:52:51 -0400

    almost done
    py-selenium linked with tf docker

---
+++ runDocker.py
@@ -0,0 +1,21 @@
+import docker
+from time import sleep
+client = docker.from_env()
+
+container = client.containers.run(
+    'dasxran/tensorflow:trainimages',
+    'python /image_classifier/scripts/return_pct.py --graph=/image_classifier/outputModel/retrained_graph.pb '
+    '--labels=/image_classifier/outputModel/retrained_labels.txt --input_layer=Placeholder '
+    '--output_layer=final_result --image=/image_classifier/testData/image1.png',
+    detach=False, auto_remove=False, remove=True, tty=True, stdin_open=True, volumes={
+        'C:/Users/user/PycharmProjects/seleniumMachineLearning/tfImageClassifier': {
+            'bind': '/image_classifier',
+            'mode': 'rw',
+        }
+    })
+
+#print(container)
+#sleep(10)
+#dockerRes = container.logs()
+print(float(container.decode().split('\r\n')[-2]))
+#container.remove()
